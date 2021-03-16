99e5a14aeb6419563785be212447cf60dad51da3
tom.chiu[邱泰詠] <tom.chiu@tomchiu.local>
2021-03-16 16:40:03 +0800

    高階課程 js 基礎

---
+++ course/high-level/javascriptBasic/topic1.js
@@ -0,0 +1,6 @@
+function divisors(integer) {
+
+};
+console.log(divisors(12)); // should return [2,3,4,6]
+console.log(divisors(25)); // should return [5]
+console.log(divisors(13)); // should return "13 is prime"
