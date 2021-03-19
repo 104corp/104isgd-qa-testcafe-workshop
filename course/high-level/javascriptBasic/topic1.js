e8ca1a617017d5a2259dd0a8eda5cf4afcae8f52
mars.wu[吳鴻君] <mars.wu@104.com.tw>
2021-03-19 16:17:07 +0800

    先上傳虛擬碼

--- course/high-level/javascriptBasic/topic1.js
+++ course/high-level/javascriptBasic/topic1.js
@@ -1,6 +1,16 @@
-function divisors(integer) {
-
+function a(integer) {
+    let arr = [];
+    for(let index = 2; index < integer; index++) {
+        if(integer%index == 0) {
+            arr.push(index);
+        }
+    }
+    if(arr.length == 0) {
+        return integer + ' is prime';
+    }
+    
+    return arr;
 };
-console.log(divisors(12)); // should return [2,3,4,6]
-console.log(divisors(25)); // should return [5]
-console.log(divisors(13)); // should return "13 is prime"
+console.log(a(12)); // should return [2,3,4,6]
+console.log(a(25)); // should return [5]
+console.log(a(13)); // should return "13 is prime"
--- course/high-level/javascriptBasic/topic2.js
+++ course/high-level/javascriptBasic/topic2.js
@@ -8,6 +8,16 @@ let target = 9;
  */
 var twoSum = function(nums, target) {
 
+let c=[];
+for(let i=0;i<nums.length-1;i++){
+    for(let j=1;j<nums.length-1;j++){
+        if(nums[i]+nums[j] == target){
+            c.push(i,j);
+            return c;
+        }
+    }
+   
+}
 };
 
 console.log(twoSum(nums, target));
