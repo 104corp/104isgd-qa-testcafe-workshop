5cfbfe7450a37579618c479a4bdba7c5cd0ee9ed
mars.wu[吳鴻君] <mars.wu@104.com.tw>
2021-03-19 16:23:20 +0800

    再上傳實際撰寫的程式

--- course/high-level/javascriptBasic/homework_marswu/homework_marswu.js
+++ course/high-level/javascriptBasic/homework_marswu/homework_marswu.js
@@ -5,3 +5,15 @@
 // 呈上,若沒大於,就將相乘後的值加在原本的空字串後
 // 每當一個num2迴圈執行完,就換行
 // 每當一個num1的迴圈執行完,就印出str
+
+let str="";
+for(let num1=1;num1<10;num1++){
+    for(let num2=1;num2<10;num2++){
+        if(num2 > num1){
+            break;
+          }
+        str+=num1 + "*" + num2 + "=" + num2*num1 + " ";
+                
+    }str+='\n';
+      
+}console.log(str);
