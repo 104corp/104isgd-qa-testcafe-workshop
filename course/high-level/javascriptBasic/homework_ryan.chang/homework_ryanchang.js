bb05f024e6c9fcece8ad1383b7cf178029bfa6cb
ryan.chang[張哲瑋] <ryan.chang@104.com.tw>
2021-03-22 18:50:49 +0800

    homework_2

--- course/high-level/javascriptBasic/homework_ryan.chang/homework_ryanchang.js
+++ course/high-level/javascriptBasic/homework_ryan.chang/homework_ryanchang.js
@@ -6,3 +6,15 @@
 //      將 a*b 的結果指定給變數 c    
 //      將 c 運算結果加上換行符號
 //      將 c 運算結果印到 console.log
+
+let c="";
+for( let a=1 ; a<=9 ; a++ ){
+    for( let b=1 ; b<=9 ; b++ ){
+        if ( b>a ){
+        break;
+         }
+    c+= a + "x" + b + "=" + (a*b) + " "
+    }
+    c=c+'\n'
+}
+console.log(c);
