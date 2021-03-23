f8f08f5b2209b80d8c6cad8f56c7f5e93b5c0ef1
jodie-hu <41200364+jodie-hu@users.noreply.github.com>
2021-03-23 16:01:54 +0800

    程式碼

--- course/high-level/javascriptBasic/homework_jodie/homework_jodie.js
+++ course/high-level/javascriptBasic/homework_jodie/homework_jodie.js
@@ -1,6 +1,14 @@
 //題目： 99 乘法表
 
-//宣告回傳字串
+
 //因數從 1 開始乘以從 1 開始的倍數
 //因數從 9 為止
-//倍數到等於因數為止
+for (let num = 1; num <= 9; num++){
+  //宣告回傳字串
+  let result = "";
+  //倍數到等於因數為止
+  for(let next = 1; next <= num; next++){
+    result = result + num + "*" + next + "=" + num * next
+  }
+    console.log(result);
+}
