60c8017f7b593cc8b8ad0e7985385b587fa91399
mars.wu[吳鴻君] <mars.wu@104.com.tw>
2021-03-24 13:12:58 +0800

    commit 訊息

--- course/high-level/javascriptBasic/homework_marswu/homework_marswu.js
+++ course/high-level/javascriptBasic/homework_marswu/homework_marswu.js
@@ -1,19 +1,15 @@
-// 先宣告一個空字串
-// 從 num1 loop開始跑進行
-// 從 num2 loop開始跑進行
-// 先判斷num2是否大於num1,若大於則跳過num2迴圈
-// 呈上,若沒大於,就將相乘後的值加在原本的空字串後
-// 每當一個num2迴圈執行完,就換行
-// 每當一個num1的迴圈執行完,就印出str
 
-let str="";
+// 從 Multiplicand loop開始跑
+// 先宣告一個空字串str
+// 從 multiplier loop開始跑,且範圍是不大於Multiplicand
+// 每跑完一次multiplier loop,就加到str後面(並加上表格"\t")
+// 每當跑完一次Multiplicand的迴圈,就印出str
+
+
 for(let num1=1;num1<10;num1++){
-    for(let num2=1;num2<10;num2++){
-        if(num2 > num1){
-            break;
-          }
-        str+=num1 + "*" + num2 + "=" + num2*num1 + " ";
-                
-    }str+='\n';
-      
-}console.log(str);
+    let str="";
+    for(let num2=1;num2<=num1;num2++){
+        str+=num1 + "*" + num2 + "=" + num2*num1 + '\t';              
+    }
+    console.log(str);  
+}
