646d6f88b3e8c42b8c3657a719605c916bd09620
ryan.chang[張哲瑋] <ryan.chang@104.com.tw>
2021-03-24 18:01:27 +0800

    homework_0324

--- course/high-level/javascriptBasic/homework_ryan.chang/homework_ryanchang.js
+++ course/high-level/javascriptBasic/homework_ryan.chang/homework_ryanchang.js
@@ -1,20 +1,13 @@
-//      宣告 c 為空字串
-//      指定 a , b 的初始值
-//      從 a 開始跑進行
-//      從 b 開始跑進行
-//      判斷若 b > a 則不繼續執行 a*b  
-//      將 a*b 的結果指定給變數 c    
-//      將 c 運算結果加上換行符號
-//      將 c 運算結果印到 console.log
-
-let c="";
-for( let a=1 ; a<=9 ; a++ ){
-    for( let b=1 ; b<=9 ; b++ ){
-        if ( b>a ){
-        break;
-         }
-    c+= a + "x" + b + "=" + (a*b) + " "
+// 從 Multiplicand 開始跑執行
+// 先宣告一個空字串str
+// 從 multiplier loop開始跑執行 ( 範圍不超過 Multiplicand )
+// 跑完一次迴圈就把值加上 stg 字串串接後面然後加上換行 "\t"
+// 跑完九次迴圈後 , 跳出並印出str
+ 
+for(let Multiplicand = 1 ; Multiplicand <= 9 ; Multiplicand++ ){
+    let str="";
+    for(let multiplier = 1 ; multiplier <= Multiplicand ; multiplier++ ){
+        str += Multiplicand + "*" + multiplier + "=" + multiplier*Multiplicand + '\t';              
     }
-    c=c+'\n'
+    console.log(str);  
 }
-console.log(c);
