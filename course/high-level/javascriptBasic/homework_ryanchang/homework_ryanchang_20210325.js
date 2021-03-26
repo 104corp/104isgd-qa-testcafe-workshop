3e9b5134df7adc5945017a69a8620d9e51d274c1
ryan.chang[張哲瑋] <ryan.chang@104.com.tw>
2021-03-26 15:26:17 +0800

    homework_0326

--- course/high-level/javascriptBasic/homework_ryanchang/homework_ryanchang_20210325.js
+++ course/high-level/javascriptBasic/homework_ryanchang/homework_ryanchang_20210325.js
@@ -1,23 +1,19 @@
- function pyramid(floor) {
-    for(let  pyramid = 1 ; pyramid <= floor ; pyramid ++ ){
-        let str="";
-        for(let span=0 ; span <= 6 - pyramid  ; span ++ )
-        str +=  " " ;
-            for(let floor = 1 ; floor <= (2*pyramid-1) ; floor ++ ){
-            str +=  "*" ; 
-            }
-        console.log(str);
+ function pyramid(floor) {  // 宣告一個 function pyramid 裡面有一個參數 floor
+    for(let  pyramid = 1 ; pyramid <= floor ; pyramid ++ ){ // pyramid 從 1 開始跑執行控制迴圈次數(金字塔的高度)
+        let str= "" ; // 設定 str 接收值
+         for(let space = 1 ; space <= ( floor - pyramid ) + 1 ; space ++ ){  // 從 space=1 開始跑執行
+            str +=  " " ;  // 把 space 加到 str 結果前面
+         }
+        for(let floor = 1 ; floor <= ( 2 * pyramid -1 ) ; floor ++ ){     // 從 floor=1 開始跑執行，且印出為奇數(EX:1,3,5...)
+            str +=  "*" ;       // 將數字結果轉成符號 *
         }
+        console.log(str);       //把最後運算結果 console.log 輸出
     }
-     pyramid(5);
+}
+    pyramid(5);
     
-     //宣告一個 function pyramid 裡面有一個參數 floor
-     //pyramid 從1開始跑執行控制迴圈次數(金字塔的高度)
-     //設定 str 接收值
-     //從 span=0 開始跑執行
-     //把 span 加到 str 結果前面
-     //從 floor=1 開始跑執行，並設定為奇數
-     //結數字結果轉成符號 *
-     //把最後運算結果 console.log 輸出
+     
+ 
+
 
 
