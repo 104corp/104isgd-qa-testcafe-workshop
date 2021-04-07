43b93cdbd4d6698fd1370900c405af4d927f5574
ryan.chang[張哲瑋] <ryan.chang@104.com.tw>
2021-04-07 15:34:26 +0800

    homework

--- course/high-level/javascriptBasic/homework_ryan.chang/homework_ryanchang.js
+++ course/high-level/javascriptBasic/homework_ryan.chang/homework_ryanchang.js
@@ -1,13 +1,13 @@
-// 從 Multiplicand 開始跑執行
+// 從 multiplicand 開始跑執行
 // 先宣告一個空字串str
-// 從 multiplier loop開始跑執行 ( 範圍不超過 Multiplicand )
+// 從 multiplier loop開始跑執行 ( 範圍不超過 multiplicand )
 // 跑完一次迴圈就把值加上 stg 字串串接後面然後加上換行 "\t"
 // 跑完九次迴圈後 , 跳出並印出str
  
-for(let Multiplicand = 1 ; Multiplicand <= 9 ; Multiplicand++ ){
+for(let multiplicand = 1 ; multiplicand <= 9 ; multiplicand++ ){
     let str="";
-    for(let multiplier = 1 ; multiplier <= Multiplicand ; multiplier++ ){
-        str += Multiplicand + "*" + multiplier + "=" + multiplier*Multiplicand + '\t';              
+    for(let multiplier = 1 ; multiplier <= multiplicand ; multiplier++ ){
+        str += multiplicand + "*" + multiplier + "=" + multiplier * multiplicand + '\t';              
     }
     console.log(str);  
 }
