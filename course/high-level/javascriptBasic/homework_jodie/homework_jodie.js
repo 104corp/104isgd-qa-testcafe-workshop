7108198107297c8c39452e7f9d5e9631363f916f
jodie-hu <41200364+jodie-hu@users.noreply.github.com>
2021-04-14 13:39:39 +0800

    Jodie 20210323 (#35)
    * 虛擬碼
    
    * 程式碼
    
    * + '\t'

---
+++ course/high-level/javascriptBasic/homework_jodie/homework_jodie.js
@@ -0,0 +1,14 @@
+//題目： 99 乘法表
+
+
+//因數從 1 開始乘以從 1 開始的倍數
+//因數從 9 為止
+for (let num = 1; num <= 9; num++){
+  //宣告回傳字串
+  let result = "";
+  //倍數到等於因數為止
+  for(let next = 1; next <= num; next++){
+    result = result + num + "*" + next + "=" + num * next + '\t'
+  }
+    console.log(result);
+}
